cc18fa7f701ceb8d808bbc635d507235bd241e24
Pedro Miranda <pedroemanuelsm0@outlook.com>
2024-06-20 23:26:06 -0300

    feat(boardgame, chess): implementing possible moves of Rook

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,8 +2,10 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
-public abstract class ChessPiece extends Piece{
+
+public abstract class ChessPiece extends Piece {
 	
 	private Color color;
 	
@@ -19,5 +21,9 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}
 	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 	
 }
