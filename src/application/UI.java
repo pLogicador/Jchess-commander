27cd1dd117c7b16b2f7cbfc1414e342bcd4a2973
Pedro Miranda <pedroemanuelsm0@outlook.com>
2024-06-21 00:14:22 -0300

    feat(application, chess[pieces]): implementing possible moves of king

--- src/application/UI.java
+++ src/application/UI.java
@@ -10,6 +10,8 @@ import chess.Color;
 public class UI {
 	
 	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+	
+	// Regular
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -20,6 +22,7 @@ public class UI {
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_WHITE = "\u001B[37m";
 
+	// Background
 	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
 	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
 	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
@@ -28,6 +31,16 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+	
+	// Bold
+    public static final String BLACK_BOLD = "\033[1;30m";  
+    public static final String RED_BOLD = "\033[1;31m";    
+    public static final String GREEN_BOLD = "\033[1;32m";  
+    public static final String YELLOW_BOLD = "\033[1;33m"; 
+    public static final String BLUE_BOLD = "\033[1;34m";   
+    public static final String PURPLE_BOLD = "\033[1;35m"; 
+    public static final String CYAN_BOLD = "\033[1;36m";   
+    public static final String WHITE_BOLD = "\033[1;37m";  
 	
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
 	public static void clearScreen() {
